bf0534660b5ab9471c1eef8d4fb7312b7725f84f
Mohammad Jami <srbackendeveloper2772@gmail.com>
2023-04-12 15:11:32 +0500

    Add Forget Password

---
+++ resources/views/auth/forgetPasswordLink.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app-auth')
+
+@section('content')
+    <div class="hero-section">
+        <div class="container">
+            <div class="row hero-content signup-hero">
+                <div class="col-lg-12">
+                    <div class="signup-form">
+                        <a href="index.php"><img src="{{asset('frontend/')}}/assets/img/logo.png"
+                                                 class="d-block mx-auto mb-3" alt=""></a>
+                        <h2>Reset Password</h2>
+                        @if (Session::has('message'))
+                          <div class="alert alert-success" role="alert">
+                              {{ Session::get('message') }}
+                          </div>
+                        @endif
+                        @include('layouts.messages')
+                        <form method="POST" action="{{ route('reset.password.post') }}" class="mt-4">
+                            @csrf
+                            <input type="hidden" name="token" value="{{ $token }}">
+                            <input type="email" name="email" class="form-control rounded-0 my-3" placeholder="Email"
+                                   value="{{ old('email') }}" required>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                            @enderror
+                            <input type="password" name="password" class="form-control rounded-0 my-3"
+                                   placeholder="Password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                            <input type="password" name="password_confirmation" class="form-control rounded-0 my-3"
+                                   placeholder="Password">
+                            @error('password_confirmation')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                            <button type="submit" class="btn btn-main w-100">
+                                {{ __('Reset Password') }}
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
