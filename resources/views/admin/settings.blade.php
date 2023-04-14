d2ae381a001b504bdc52c497801f505f55951938
hirfan875@gmail.com <hirfan875@gmail.com>
2023-04-14 15:34:47 +0500

    fixes

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -19,22 +19,23 @@
                             @csrf
                             <div class="form-row">
                                 <div class="col-md-6 mb-3">
-                                    <label for="validationCustom02">Contact</label>
-                                    <input type="text" class="form-control" name="contact" id="validationCustom02" placeholder="enter contact" value="{{ $contact??'' }}" required>
+                                    <label for="contact_no">Contact</label>
+                                    <input type="text" class="form-control" name="contact" id="contact_no"
+                                           placeholder="Enter contact" value="{{ $contact??'' }}" required>
                                 </div>
                                 <div class="col-md-6 mb-3">
-                                    <label for="validationCustom02">Email</label>
-                                    <input type="text" class="form-control" name="email" id="validationCustom02" placeholder="enter email" value="{{ $email??'' }}" required>
+                                    <label for="email">Email</label>
+                                    <input type="text" class="form-control" name="email" id="email"
+                                           placeholder="Enter email" value="{{ $email??'' }}" required>
                                 </div>
                                 <div class="col-md-12 mb-3">
-                                    <label for="validationCustom02">Address</label>
-                                    <input type="text" class="form-control" name="address" id="validationCustom02" placeholder="enter address" value="{{ $address??'' }}" required>
+                                    <label for="address">Address</label>
+                                    <input type="text" class="form-control" name="address" id="address"
+                                           placeholder="Enter address" value="{{ $address??'' }}" required>
                                 </div>
                                 <div class="col-md-12 mb-3">
-                                    <label for="logo">Footer Description</label>
-                                    <div class="card-body editor-desc">
-                                        <textarea class="form-control" name="footer_description" id="summernote_1" rows="8">{{ $footer_description??'' }}</textarea>
-                                    </div>
+                                    <label for="description">Footer Description</label>
+                                    <textarea id="description" class="form-control" name="footer_description" rows="6">{{ $footer_description??'' }}</textarea>
                                 </div>
                                 <div class="col-md-12 mb-3">
                                     <label for="logo">Logo</label>
@@ -48,7 +49,7 @@
                                 </div>
 
                             </div>
-                            <button class="btn btn-primary" type="submit">Save</button>
+                            <button class="btn btn-success" type="submit">Save</button>
                         </form>
                     </div>
                 </section>
