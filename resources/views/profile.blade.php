f60dc9bb9b0f7a4cf7fc49c100e1b19a84a73226
Irfan <hirfan875@gmail.com>
2023-04-17 16:33:54 +0500

    fixes

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -33,29 +33,19 @@
                             <div class="mb-3">
                                 <label for="first_name" class="form-label">First Name</label>
                                 <input id="first_name" type="text" class="form-control" placeholder="First Name"
-                                       name="first_name" value="{{ $user->first_name??'' }}">
+                                       name="first_name" value="{{ old('first_name', $user->first_name) }}">
                             </div>
                             <div class="mb-3">
                                 <label for="last_name" class="form-label">Last Name</label>
                                 <input id="last_name" type="text" class="form-control"
                                        placeholder="Last Name"
-                                       name="last_name" value="{{ $user->last_name??'' }}">
+                                       name="last_name" value="{{ old('last_name', $user->last_name) }}">
                             </div>
                             <div class="mb-3">
                                 <label for="contact_no" class="form-label">Contact Number</label>
                                 <input id="contact_no" type="number" class="form-control" placeholder="Phone Number"
-                                       name="contact_no" value="{{ $user->contact_no??'' }}">
+                                       name="contact_no" value="{{ old('contact_no', $user->contact_no) }}">
                             </div>
-                           {{-- <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input id="password" type="password" class="form-control" name="password"
-                                       placeholder="Password">
-                            </div>
-                            <div class="mb-3">
-                                <label for="confirm_password" class="form-label">Confirm Password</label>
-                                <input id="confirm_password" type="password" class="form-control" name="confirm_password"
-                                       placeholder="Confirm Password">
-                            </div>--}}
                             <button type="submit" class="btn btn-main" type="submit">Update Profile</button>
                         </form>
                     </div>
