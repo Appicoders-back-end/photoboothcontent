9bbd9420e2de468564c77c85b73d06315b5a9182
mparyal371 <mparyal371@gmail.com>
2023-04-28 14:48:42 +0500

    admin orders

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -24,4 +24,26 @@ class OrderController extends Controller
         $order = Order::with('items.product', 'user')->findOrFail($id);
         return view('admin.orders.show', compact('order'));
     }
+
+    public function orderStatus(Request $request , $id){
+        try {
+            $order = Order::find($id);
+
+            if ($request->status === Order::PENDING){
+                $order->status = Order::PENDING;
+            }elseif ($request->status === Order::PROCESSING){
+                $order->status = Order::PROCESSING;
+            }elseif ($request->status === Order::COMPLETED){
+                $order->status = Order::COMPLETED;
+            }elseif ($request->status === Order::CANCEL){
+                $order->status = Order::CANCEL;
+            }
+
+            $order->save();
+            return redirect()->route('admin.orders.index')->with('success', 'Order Status has been updated successfully');
+
+        } catch (\Exception $exception) {
+            return redirect()->back()->with('error', $exception->getMessage());
+        }
+    }
 }
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -27,13 +27,43 @@
                                     <tr class="gradeX">
                                         <td>{{ $order->order_no ?? '-' }}</td>
                                         <td>{{ $order->user ? $order->user->name : '-' }}</td>
-                                        <td>{{ $order->paid_amount ?? 'N/A' }}</td>
-                                        <td>
+                                        <td>${{ number_format($order->paid_amount,2)?? '-' }}</td>
+                                       {{-- <td>
                                             @if($order->status == "active")
                                                 <span class="text-success">{{ ucwords($order->status) }}</span>
                                             @else
                                                 <span class="text-danger">{{ ucwords($order->status) }}</span>
                                             @endif
+                                        </td>--}}
+                                        <td width="1%">
+                                            @if($order->status == "completed")
+                                                <label class="badge @if($order->status === "completed") badge-success @endif ">{{ strtoupper($order->status)??'-' }}</label>
+                                            @endif
+                                            @if($order->status == "cancel")
+                                                    <label class="badge @if($order->status === "cancel") badge-warning @endif ">{{ strtoupper($order->status)??'-' }}</label>
+                                            @endif
+                                            <div class="col-md-3 mb-3 @if($order->status == "completed" || $order->status == "cancel") d-none @endif">
+                                                <form action="{{ route('admin.orders.status',$order->id) }}"
+                                                      method="GET">
+                                                    <select class="form-control mb-2" id="status" name="status">
+                                                        <option value="" disabled selected>Select Status</option>
+                                                        <option style="background-color:transparent;" value="pending"
+                                                                @if($order->status == "processing") disabled @endif
+                                                                @if($order->status == 'pending') selected @endif>
+                                                            PENDING
+                                                        </option>
+                                                        <option value="processing"
+                                                                @if($order->status == "processing") selected @endif>PROCESSING
+                                                        </option>
+                                                        <option value="completed"
+                                                                @if($order->status == "completed") selected @endif>COMPLETED
+                                                        </option>
+                                                        <option value="cancel"
+                                                                @if($order->status == "cancel") selected @endif>CANCEL
+                                                        </option>
+                                                    </select>
+                                                </form>
+                                            </div>
                                         </td>
                                         <td>{{ date('F d, Y', strtotime($order->created_at))??'-'}} </td>
                                         <td>
@@ -51,4 +81,13 @@
         </div>
         <!-- page end-->
     </section>
+@endsection
+@section('script')
+    <script>
+        $(document).ready(function () {
+            $(document).on('change', '#status', function () {
+                this.form.submit();
+            });
+        });
+    </script>
 @endsection
