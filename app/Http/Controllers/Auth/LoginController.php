cc7e190f036f45f1be72749dbf2f153f3bb02f6d
Irfan <hirfan875@gmail.com>
2023-04-28 18:01:08 +0500

    fixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,7 +43,15 @@ class LoginController extends Controller
     public function attemptLogin(Request $request)
     {
         return $this->guard()->attempt(
-            ['email' => $request->email, 'password' => $request->password, 'role' => User::ROLE_CUSTOMER, 'status' => User::ACTIVE]
+            ['email' => $request->email, 'password' => $request->password, 'role' => User::ROLE_CUSTOMER]
         );
+    }
+
+    public function authenticated(Request $request, $user)
+    {
+        if ($user->status !== User::ACTIVE) {
+            auth()->logout();
+            return redirect('login')->withErrors('Your Account is inactive, please contact to admin.');
+        }
     }
 }
