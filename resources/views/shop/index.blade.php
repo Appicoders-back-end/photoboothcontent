f23d132957706bab5621cb0dca7c511bf88f42b9
Irfan <hirfan875@gmail.com>
2023-05-11 19:44:43 +0500

    fixes

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -36,7 +36,7 @@
             @forelse($products as $key => $product)
                 <div class="col-lg-4 col-md-6">
                     <div class="product-card">
-                        <img src="{{ $product->getImages()[0] }}" alt="" class="product-img">
+                        <a href="{{ route('shop.product.detail',$product->id) }}"><img src="{{ $product->getImages()[0] }}" alt="" class="product-img"></a>
                         <h4 class="product-name">{{ $product->title }}</h4>
                         <p class="product-price">${{ number_format($product->price,2) }}</p>
 {{--                        <p class="product-desc">{!! $product->description !!}</p>--}}
