207145c2825729966ac6e408fbbf8d7888cc30fb
Irfan <hirfan875@gmail.com>
2023-05-15 18:26:13 +0500

    fixes

--- resources/views/myCoupons.blade.php
+++ resources/views/myCoupons.blade.php
@@ -24,38 +24,62 @@
                                         <td>{{$coupon->coupon ? $coupon->coupon->name : '-'}}</td>
                                         <td>{{$coupon->code}}</td>
                                         <td>
-                                            <div class="row text-center">
+                                            <table class="table text-center borderless">
+                                                <tr>
+                                                    <td>Photo</td>
+                                                    <td>Video</td>
+                                                    <td>Document</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>{{$coupon->total_images}}</td>
+                                                    <td>{{$coupon->total_videos}}</td>
+                                                    <td>{{$coupon->total_documents}}</td>
+                                                </tr>
+                                            </table>
+                                            {{--<div class="row text-center">
                                                 <div class="col-4">
                                                     <p>Photo</p>
                                                     <p>{{$coupon->total_images}}</p>
                                                 </div>
                                                 <div class="col-4">
                                                     <p>Video</p>
-                                                    <p>{{$coupon->total_images}}</p>
+                                                    <p>{{$coupon->total_videos}}</p>
                                                 </div>
                                                 <div class="col-4">
                                                     <p>Doc</p>
                                                     <p>{{$coupon->total_documents}}</p>
                                                 </div>
-                                            </div>
+                                            </div> --}}
                                         </td>
                                         <td>
-                                            <div class="row text-center bg-secondary-subtle">
-                                                <div class="col-4">
-                                                    <p>Photo</p>
-                                                    <p>{{$coupon->downloaded_images}}</p>
-                                                </div>
-                                                <div class="col-4">
-                                                    <p>Video</p>
-                                                    <p>{{$coupon->downloaded_videos}}</p>
-                                                </div>
-                                                <div class="col-4">
-                                                    <p>Doc</p>
-                                                    <p>{{$coupon->downloaded_documents}}</p>
-                                                </div>
-                                            </div>
+                                            <table class="table text-center borderless">
+                                                <tr>
+                                                    <td>Photo</td>
+                                                    <td>Video</td>
+                                                    <td>Document</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>{{$coupon->downloaded_images}}</td>
+                                                    <td>{{$coupon->downloaded_videos}}</td>
+                                                    <td>{{$coupon->downloaded_documents}}</td>
+                                                </tr>
+                                            </table>
+{{--                                            <div class="row text-center bg-secondary-subtle">--}}
+{{--                                                <div class="col-4">--}}
+{{--                                                    <p>Photo</p>--}}
+{{--                                                    <p>{{$coupon->downloaded_images}}</p>--}}
+{{--                                                </div>--}}
+{{--                                                <div class="col-4">--}}
+{{--                                                    <p>Video</p>--}}
+{{--                                                    <p>{{$coupon->downloaded_videos}}</p>--}}
+{{--                                                </div>--}}
+{{--                                                <div class="col-4">--}}
+{{--                                                    <p>Doc</p>--}}
+{{--                                                    <p>{{$coupon->downloaded_documents}}</p>--}}
+{{--                                                </div>--}}
+{{--                                            </div>--}}
                                         </td>
-                                        <td class="text-success fw-bold">{{$coupon->subscription_id ? "Purchased via subscription" : '$'.$coupon->price}}</td>
+                                        <td class="text-success fw-bold" style="white-space: nowrap">{{$coupon->subscription_id ? "Purchased via subscription" : '$'.$coupon->price}}</td>
                                     </tr>
                                 @endforeach
                                 </tbody>
