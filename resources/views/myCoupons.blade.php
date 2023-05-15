d16d0590e2e39da2955cd0dde6cca19da62fd0a7
Irfan <hirfan875@gmail.com>
2023-05-15 18:27:04 +0500

    fixes

--- resources/views/myCoupons.blade.php
+++ resources/views/myCoupons.blade.php
@@ -36,20 +36,6 @@
                                                     <td>{{$coupon->total_documents}}</td>
                                                 </tr>
                                             </table>
-                                            {{--<div class="row text-center">
-                                                <div class="col-4">
-                                                    <p>Photo</p>
-                                                    <p>{{$coupon->total_images}}</p>
-                                                </div>
-                                                <div class="col-4">
-                                                    <p>Video</p>
-                                                    <p>{{$coupon->total_videos}}</p>
-                                                </div>
-                                                <div class="col-4">
-                                                    <p>Doc</p>
-                                                    <p>{{$coupon->total_documents}}</p>
-                                                </div>
-                                            </div> --}}
                                         </td>
                                         <td>
                                             <table class="table text-center borderless">
@@ -64,20 +50,6 @@
                                                     <td>{{$coupon->downloaded_documents}}</td>
                                                 </tr>
                                             </table>
-{{--                                            <div class="row text-center bg-secondary-subtle">--}}
-{{--                                                <div class="col-4">--}}
-{{--                                                    <p>Photo</p>--}}
-{{--                                                    <p>{{$coupon->downloaded_images}}</p>--}}
-{{--                                                </div>--}}
-{{--                                                <div class="col-4">--}}
-{{--                                                    <p>Video</p>--}}
-{{--                                                    <p>{{$coupon->downloaded_videos}}</p>--}}
-{{--                                                </div>--}}
-{{--                                                <div class="col-4">--}}
-{{--                                                    <p>Doc</p>--}}
-{{--                                                    <p>{{$coupon->downloaded_documents}}</p>--}}
-{{--                                                </div>--}}
-{{--                                            </div>--}}
                                         </td>
                                         <td class="text-success fw-bold" style="white-space: nowrap">{{$coupon->subscription_id ? "Purchased via subscription" : '$'.$coupon->price}}</td>
                                     </tr>
